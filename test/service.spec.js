85690cb38c243e45d62df8a126e96869aea77331
Somodi Viktor <somodi.viktor@gmail.com>
2015-12-29 13:38:41 +0100

    test(service): add for #isOff and #isOn

--- test/service.spec.js
+++ test/service.spec.js
@@ -5,13 +5,35 @@ import { FlipperService } from '../lib/service';
 
 describe('Flipper Service', function() {
 
+  let service;
+
+  beforeEach(function() {
+    service = new FlipperService();
+    service.setFlippers(['testflipper']);
+  });
+
   describe('#isOn', function() {
 
-    it('should translate the given simple text', function() {
-      let service = new FlipperService();
-      service.setFlippers(['testflipper']);
-
+    it('should be true if the given flipper is on', function() {
       expect(service.isOn('testflipper')).to.be.true;
+    });
+
+
+    it('should be false if the given flipper is off', function() {
+      expect(service.isOn('otherflipper')).to.be.false;
+    });
+
+  });
+
+  describe('#isOff', function() {
+
+    it('should be false if the given flipper is on', function() {
+      expect(service.isOff('testflipper')).to.be.false;
+    });
+
+
+    it('should be true if the given flipper is off', function() {
+      expect(service.isOff('otherflipper')).to.be.true;
     });
 
   });
