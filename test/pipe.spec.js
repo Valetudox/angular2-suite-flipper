ac3964bc6c0377a0c24dda51eea34794b8c67d85
Somodi Viktor <somodi.viktor@gmail.com>
2015-12-29 13:43:35 +0100

    test(pipes): add for #transform

---
+++ test/pipe.spec.js
@@ -0,0 +1,58 @@
+import 'es6-shim';
+import 'reflect-metadata';
+const { expect } = require('chai');
+import { FlipperService, FlipperIsOffPipe, FlipperIsOnPipe } from '../lib';
+
+describe('Pipes', function() {
+
+  let service;
+  let pipe;
+
+  beforeEach(function () {
+    service = new FlipperService();
+    service.setFlippers(['testflipper']);
+  });
+
+  describe('Is On Pipe', function() {
+
+    beforeEach(function () {
+      pipe = new FlipperIsOnPipe(service);
+    });
+
+    describe('#transform', function () {
+
+      it('should be true if the given flipper is on', function () {
+        expect(pipe.transform('testflipper')).to.be.true;
+      });
+
+
+      it('should be false if the given flipper is off', function () {
+        expect(pipe.transform('other')).to.be.false;
+      });
+
+    });
+
+  });
+
+  describe('Is Off Pipe', function() {
+
+    beforeEach(function () {
+      pipe = new FlipperIsOffPipe(service);
+    });
+
+    describe('#transform', function () {
+
+      it('should be false if the given flipper is on', function () {
+        expect(pipe.transform('testflipper')).to.be.false;
+      });
+
+
+      it('should be true if the given flipper is off', function () {
+        expect(pipe.transform('other')).to.be.true;
+      });
+
+    });
+
+  })
+
+});
