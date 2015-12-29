7b4a51e3d4cd017e64f99fa47a9af3fd7ad6780c
Somodi Viktor <somodi.viktor@gmail.com>
2015-12-29 06:14:31 +0100

    feat(build): use babel

---
+++ test/service.spec.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import 'es6-shim';
+import 'reflect-metadata';
+const { expect } = require('chai');
+import { FlipperService } from '../lib/service';
+
+describe('Flipper Service', function() {
+
+  describe('#isOn', function() {
+
+    it('should translate the given simple text', function() {
+      let service = new FlipperService();
+      service.setFlippers(['testflipper']);
+
+      expect(service.isOn('testflipper')).to.be.true;
+    });
+
+  });
+
+});
